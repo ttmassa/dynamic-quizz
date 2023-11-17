fa23c9e5378239471d2f4f979c8ef705a571d58b
Massa Timothée <timothee.massa.perso@gmail.com>
2023-11-17 13:04:39 +0100

    question card styled + difficulty level added

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function Question(props) {
     return (
-        <section>
+        <section className='question'>
+            <img
+                src={props.level}
+                alt="question's difficulty"
+                className='question--level'
+            />
             <h2>{props.question}</h2>
             <p>{props.answer}</p>
         </section>
-    )
+    );
 }
